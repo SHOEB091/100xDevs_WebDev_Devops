adcd03a14426c50d8cf0dd9f364684a21ee442d7
shoeb <wiqbal091@gmail.com>
2025-01-12 03:00:55 +0530

    hard Assignment Complete

--- Assignment/week-2/week-2-js/hard/todo-list.js
+++ Assignment/week-2/week-2-js/hard/todo-list.js
@@ -11,7 +11,77 @@
 */
 
 class Todo {
-  
+  constructor() {
+    this.todos = [];
+  }
+
+  // Adds a todo to the list of todos
+  add(todo) {
+    this.todos.push(todo);
+  }
+
+  // Removes a todo from the list of todos by index
+  remove(indexOfTodo) {
+    if (indexOfTodo >= 0 && indexOfTodo < this.todos.length) {
+      this.todos.splice(indexOfTodo, 1);
+    } else {
+      throw new Error("Invalid index");
+    }
+  }
+
+  // Updates a todo at the given index
+  update(index, updatedTodo) {
+    if (index >= 0 && index < this.todos.length) {
+      this.todos[index] = updatedTodo;
+    } else {
+      throw new Error("Invalid index");
+    }
+  }
+
+  // Returns all todos
+  getAll() {
+    return this.todos;
+  }
+
+  // Returns a todo at the given index
+  get(indexOfTodo) {
+    if (indexOfTodo >= 0 && indexOfTodo < this.todos.length) {
+      return this.todos[indexOfTodo];
+    } else {
+      throw new Error("Invalid index");
+    }
+  }
+
+  // Deletes all todos
+  clear() {
+    this.todos = [];
+  }
 }
+
+// Example usage
+const todoList = new Todo();
+
+// Add todos
+todoList.add("Learn JavaScript");
+todoList.add("Learn React");
+todoList.add("Build a project");
+
+// Get all todos
+console.log(todoList.getAll()); // Output: ["Learn JavaScript", "Learn React", "Build a project"]
+
+// Get a specific todo
+console.log(todoList.get(1)); // Output: "Learn React"
+
+// Update a todo
+todoList.update(1, "Learn Redux");
+console.log(todoList.getAll()); // Output: ["Learn JavaScript", "Learn Redux", "Build a project"]
+
+// Remove a todo
+todoList.remove(0);
+console.log(todoList.getAll()); // Output: ["Learn Redux", "Build a project"]
+
+// Clear all todos
+todoList.clear();
+console.log(todoList.getAll()); // Output: []
 
 module.exports = Todo;
