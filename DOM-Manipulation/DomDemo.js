a50cbc9f4f583e046e333a9da8b933b48a1abf29
shoeb <wiqbal091@gmail.com>
2025-02-02 01:37:36 +0530

    Dom Manipulation

---
+++ DOM-Manipulation/DomDemo.js
@@ -0,0 +1,17 @@
+// The Dom or Document Object Model , is a programming interface for web documents.
+// It reperesents the structure of a when page as a tree of objects.
+
+// Why DOM?
+// The DOM abstracts the structure of the document into a tree of objects, allowing scripts to manipulate the content and structure dynamically. This abstraction enables more complex interactions and functionalities beyond just static HTML.
+
+
+
+
+
+
+
+
+
+
+
+
