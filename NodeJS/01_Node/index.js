0c8bbb31d3dde36103b571eab841f0eba219621b
shoeb <wiqbal091@gmail.com>
2025-02-26 23:29:47 +0530

    express syntax

--- NodeJS/01_Node/index.js
+++ NodeJS/01_Node/index.js
@@ -2,6 +2,15 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.get("/route-handler",function(req,res){
+    // header , body , querry parameters
+    // do machine learning model
+    res.json({
+        name:"shoeb iqbal",
+        age:21,
+    })
+
+})
 
 app.get('/',function(req,res){
     res.send("Hello World!")
